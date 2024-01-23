4c434d3be7266ee0a04321e6287a840dcbfcf469
RusssianMusketeer <a.devetriev@gmail.com>
2024-01-23 17:20:40 -0500

    test2

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'https://russsianmusketeer.github.io/Galleria-slideshow/'
+    baseUrl: 'http://localhost:8080/Galleria-slideshow/'
   }
 })
